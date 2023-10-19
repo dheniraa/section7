bcdde901672a5d632448623647d62301a076bb25
dheniraa <dheeniiraa24@gmail.com>
2023-10-19 16:17:35 +0700

    update progres 3

--- lib/app/modules/product/views/product_view.dart
+++ lib/app/modules/product/views/product_view.dart
@@ -11,13 +11,111 @@ class ProductView extends GetView<ProductController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('ProductView'),
-        centerTitle: true,
+        backgroundColor: Colors.white,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Image.asset('assets/logo.png'),
+            Text(
+              'Fake Shop',
+              style:
+                  TextStyle(color: Colors.purple, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
       ),
-      body: Center(
-        child: Text(
-          'ProductView is working',
-          style: TextStyle(fontSize: 20),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Stack(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Card(
+                  child: Container(
+                    height: 200,
+                    child: Image.asset(
+                      'assets/images/8.png',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'Cotton-gabardine trench cape',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Row(
+                  children: List.generate(5, (index) {
+                    return Icon(
+                      Icons.star,
+                      color: Colors.amber,
+                    );
+                  }),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    '\$300',
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'Jacket',
+                  ),
+                ),
+                SizedBox(height: 20),
+                SizedBox(
+                  width: 200,
+                  child: ElevatedButton.icon(
+                    onPressed: () {},
+                    icon: Icon(Icons.attach_money),
+                    label: Text(""),
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                    ),
+                  ),
+                ),
+                Spacer(),
+              ],
+            ),
+            Positioned(
+              bottom: 0,
+              right: 0,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.yellow,
+                        shape: CircleBorder(),
+                      ),
+                      child: Icon(Icons.edit, color: Colors.white),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.red,
+                        shape: CircleBorder(),
+                      ),
+                      child: Icon(Icons.delete, color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
