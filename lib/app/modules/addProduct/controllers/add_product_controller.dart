2f8d97ad72b6baf7406ea779453d6d97a393ec09
dheniraa <dheeniiraa24@gmail.com>
2023-10-25 09:08:53 +0700

    progres

--- lib/app/modules/addProduct/controllers/add_product_controller.dart
+++ lib/app/modules/addProduct/controllers/add_product_controller.dart
@@ -1,7 +1,58 @@
 import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+import '../../../data/product_model.dart';
+import 'package:section7/app/data/service_api.dart';
+
+class FormController extends GetxController {
+  ServiceApi serviceApi = ServiceApi();
+
+  TextEditingController titleC = TextEditingController();
+  TextEditingController categoryC = TextEditingController();
+  TextEditingController priceC = TextEditingController();
+  TextEditingController descriptionC = TextEditingController();
+
+  bool checkIsDouble(String? text) {
+    try {
+      double.parse(text ?? '');
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  modelToController(Product product) {
+    titleC.text = product.title ?? '';
+    categoryC.text = product.category ?? '';
+    priceC.text = (product.price ?? '').toString();
+    descriptionC.text = product.description ?? '';
+  }
+
+  controllerToModel(Product product) {
+    product.title = titleC.text;
+    product.category = categoryC.text;
+    product.price = double.tryParse(priceC.text);
+    product.description = descriptionC.text;
+    return Product;
+  }
+
+  Future storeUser(Product product, bool isUpdate) async {
+    try {
+      product = controllerToModel(product);
+      isUpdate == false
+          ? await serviceApi.createProduct(product)
+          : await serviceApi.updateProduct(product);
+      Get.back();
+      Get.snackbar('Success', 'Product berhasil disimpan');
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+}
+
 
 class AddProductController extends GetxController {
   //TODO: Implement AddProductController
+  
 
   final count = 0.obs;
   @override
@@ -21,3 +72,5 @@ class AddProductController extends GetxController {
 
   void increment() => count.value++;
 }
+
+
--- lib/app/modules/product/controllers/product_controller.dart
+++ lib/app/modules/product/controllers/product_controller.dart
@@ -1,5 +1,27 @@
 import 'package:get/get.dart';
 
+import '../../../data/product_model.dart';
+import '../../../data/service_api.dart';
+
+class HomeController extends GetxController {
+  RxList<Product> listProduct = <Product>[].obs;
+
+  final ServiceApi serviceApi = ServiceApi();
+
+  Future<List<Product>> loadProduct() async {
+    return listProduct.value = await serviceApi.getProduct();
+  }
+
+  Future<void> deleteUser(Product product) async {
+    try {
+      await serviceApi.deleteProduct(Product.id.toString());
+      Get.snackbar('Success', 'Product berhasil dihapus');
+    } catch (e) {
+      print(e);
+    }
+  }
+}
+
 class ProductController extends GetxController {
   //TODO: Implement ProductController
 
